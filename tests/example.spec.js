38f45d3d37dc4237b7015b23133565b8360dd268
marielle <samanmarielle03@gmail.com>
2025-06-29 23:35:12 +0800

    added test files

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,26 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('verify that user is able to log in with valid credentials', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  
+  await page.locator('[data-test="username"]').fill('problem_user');
+  
+  await page.locator('[data-test="password"]').fill('secret_sauce');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await page.locator('[data-test="login-button"]').click();
+
+  await expect(page.locator('[data-test="primary-header"]')).toContainText('Swag Labs');
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('verify that user is unable to log in with invalid credentials', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  
+  await page.locator('[data-test="username"]').fill('test_user');
+  
+  await page.locator('[data-test="password"]').fill('secret_sauce');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.locator('[data-test="login-button"]').click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface: Username and password do not match any user in this service');
 });
---
+++ tests/features/registration.spec.js
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('https://parabank.parasoft.com/parabank/index.htm');
+    await page.getByRole('link', { name: 'Admin Page' }).click();
+    await page.getByRole('button', { name: 'Clean' }).click();
+});
+
+test('Verify that user is able to register an account', async ({ page }) => {
+  await page.goto('https://parabank.parasoft.com/parabank/index.htm');
+  await page.getByRole('link', { name: 'Register' }).click();
+  await page.locator('[id="customer.firstName"]').fill('Marielle');
+  await page.locator('[id="customer.lastName"]').fill('Test');
+  await page.locator('[id="customer.address.street"]').fill('Cornelia St');
+  await page.locator('[id="customer.address.city"]').fill('New York');
+  await page.locator('[id="customer.address.state"]').fill('New York');
+  await page.locator('[id="customer.address.zipCode"]').fill('10014');
+  await page.locator('[id="customer.phoneNumber"]').fill('13235103202');
+  await page.locator('[id="customer.ssn"]').fill('987654');
+  await page.locator('[id="customer.username"]').fill('marielle.test');
+  await page.locator('[id="customer.password"]').fill('testing123');
+  await page.locator('#repeatedPassword').fill('testing123');
+
+  await page.getByRole('button', { name: 'Register' }).click();
+  await expect(page.locator('h1.title')).toBeVisible();
+  await expect(page.locator('h1.title')).toContainText(/Welcome/);
+});
